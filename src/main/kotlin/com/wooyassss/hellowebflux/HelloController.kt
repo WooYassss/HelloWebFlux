37e98712e5bee9d8564bd28dc757fe5df1e66763
GGos3 <janpll0421@gmail.com>
2023-10-14 02:30:00 +0900

    feat: HelloController 추가

---
+++ src/main/kotlin/com/wooyassss/hellowebflux/HelloController.kt
@@ -0,0 +1,18 @@
+package com.wooyassss.hellowebflux
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    // suspend를 사용한 코루틴 사용
+
+    @GetMapping("/hello")
+    suspend fun hello(
+        @RequestParam(required = false) name: String = "GGos3"
+    ): String {
+        return "$name 님 안녕하세요."
+    }
+}
